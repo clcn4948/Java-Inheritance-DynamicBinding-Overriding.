e38dca87e125e2582b079bdfdff30647d25a07c8
donna <dwchow@ucdavis.edu>
2014-11-02 12:54:08 -0800

    Part 1: Basic Derivation

---
+++ part1/Constant.java
@@ -0,0 +1,6 @@
+/*  Abstract classes can't be called by an instance, but can be subclassed
+    Usually used to have a common functionality with a collection of children abstract classes
+ */
+
+public abstract class Constant {
+}
